0b6d5385b2f2b0d7c00f253b2b6842fb85c39e8a
Tiaan Le Grange <72667435@mylife.unisa.ac.za>
2020-12-02 16:09:50 +0200

    push

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
 using MQTTnet.Client.Options;
 using MQTTnet.Formatter;
 using MQTTnet.Protocol;
@@ -14,18 +17,24 @@ namespace MQTT
 {
     class Program
     {
+        private const int Limmit = 10000;
         static async Task Main(string[] args)
         {
-            var limit = 10000;
-            var id = 0;
+            var count = 0;
 
-            while (id < limit)
+            while (Mqtt.Clients.Count < Limmit)
             {
-                await Mqtt.Connect(Guid.NewGuid().ToString());
-                Console.WriteLine($"Connected Clients: {Mqtt.clients.Count}");
+                var id = Guid.NewGuid().ToString();
+                await Mqtt.Connect(id);
             }
 
-            Console.WriteLine($"Connected Clients: {Mqtt.clients.Count}");
+            Console.WriteLine($"{DateTime.Now:g} - Connection limit of {Limmit} reached.");
+            Console.WriteLine($"Press ENTER to exit....");
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine($"Press ENTER to exit....");
+            }
         }
     }
 
@@ -33,11 +42,13 @@ namespace MQTT
     public static class Mqtt
     {
         private const string Host = "102.133.134.75";
+        //private const string Host = "127.0.0.1";
         private const int Port = 1883;
         private const string UserName = "test";
         private const string Password = "Test1234";
-        public static MqttFactory factory = new MqttFactory();
-        public static ConcurrentBag<IMqttClient> clients = new ConcurrentBag<IMqttClient>();
+
+        private static readonly MqttFactory Factory = new MqttFactory();
+        public static readonly Dictionary<string,IMqttClient> Clients = new Dictionary<string,IMqttClient>();
 
         public static async Task Connect(string clientId)
         {
@@ -56,7 +67,8 @@ namespace MQTT
                         IgnoreCertificateChainErrors = true,
                         IgnoreCertificateRevocationErrors = true,
                         AllowUntrustedCertificates = true
-                    }
+                    },
+                    BufferSize = 2048
                 },
                 Credentials = new MqttClientCredentials
                 {
@@ -65,16 +77,13 @@ namespace MQTT
                 },
                 CleanSession = true,
                 KeepAlivePeriod = TimeSpan.FromMinutes(2),
-                CommunicationTimeout = TimeSpan.FromSeconds(30),
-                WillMessage = new MqttApplicationMessageBuilder()
-                    .WithTopic("test")
-                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtMostOnce)
-                    .Build()
+                CommunicationTimeout = TimeSpan.FromSeconds(10)
             };
 
 
-            var client = factory.CreateMqttClient();
-
+            var client = Factory.CreateMqttClient();
+            client.ConnectedHandler = new MqttClientConnectedHandlerDelegate((args) => OnConnected(args,client));
+            client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate((args) => OnDisconnected(args,clientId));
             try
             {
                 await client.ConnectAsync(options);
@@ -85,8 +94,24 @@ namespace MQTT
                 Console.WriteLine(exception);
                 Console.ForegroundColor = ConsoleColor.White;
             }
+        }
 
-            clients.Add(client);
+        private static void OnConnected(MqttClientConnectedEventArgs x, IMqttClient client)
+        {
+            Clients.Add(client.Options.ClientId,client);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{DateTime.Now:g} - {client.Options.ClientId} Connected");
+            Console.WriteLine($"{DateTime.Now:g} -- {Clients.Count} Clients Connected");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void OnDisconnected(MqttClientDisconnectedEventArgs x, string clientId)
+        {
+            Clients.Remove(clientId);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{DateTime.Now:g} - {clientId} Disconnected");
+            Console.WriteLine($"{DateTime.Now:g} -- {Clients.Count} Clients Connected");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
